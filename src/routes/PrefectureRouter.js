08679ad3466fe8d32d5da4a2283965a98b831eb3
FreddySnzz <fredsonluiz.03@gmail.com>
2022-04-22 20:26:30 -0300

    feat: added celebrate middleware in the PrefectureRouter

--- src/routes/PrefectureRouter.js
+++ src/routes/PrefectureRouter.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const RouterForPrefecture = express.Router();
-//const { celebrate, Segments, Joi } = require("celebrate");
+const { celebrate, Segments, Joi } = require("celebrate");
 const PrefectureController = require('../controllers/PrefectureController');
-//const AuthMiddleware = require('../middleware/AuthMiddleware');
 
 RouterForPrefecture.get(
   '/',
@@ -11,11 +10,67 @@ RouterForPrefecture.get(
 
 RouterForPrefecture.post(
   '/register',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      cep: Joi.number().min(8).required(),
+      uf: Joi.string().min(2).required(),
+      city: Joi.string().required(),
+      district: Joi.string().required(),
+      address: Joi.string().required(),
+      address_number: Joi.string().required(),
+      complement: Joi.string().required(),
+      mayor_name: Joi.string().required(),
+      treasury_secretariat_name: Joi.string().required(),
+      secretary_name: Joi.string().required(),
+      secretary_position: Joi.string().required(),
+      collection_sector: Joi.string().required(),
+      responsible_for_the_collection_sector: Joi.string().required(),
+      position_of_the_responsible_for_the_collection_sector: Joi.string().required(),
+      taxman_position: Joi.string().required(),
+      taxman_name: Joi.string().required(),
+      taxman_registration: Joi.string().required(),
+      technical_contact_name: Joi.string().required(),
+      technical_telephone: Joi.number().min(8).required(),
+      technical_cellphone: Joi.number().min(9).required(),
+      contact_email: Joi.string().required(),
+      prefecture_telephone: Joi.number().min(8).required(),
+      prefecture_email: Joi.string().required(),
+    }),
+  }),
   PrefectureController.registerPrefecture
 );
 
 RouterForPrefecture.patch(
   '/edit',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      cep: Joi.number().min(8).required(),
+      uf: Joi.string().min(2).required(),
+      city: Joi.string().required(),
+      district: Joi.string().required(),
+      address: Joi.string().required(),
+      address_number: Joi.string().required(),
+      complement: Joi.string().required(),
+      mayor_name: Joi.string().required(),
+      treasury_secretariat_name: Joi.string().required(),
+      secretary_name: Joi.string().required(),
+      secretary_position: Joi.string().required(),
+      collection_sector: Joi.string().required(),
+      responsible_for_the_collection_sector: Joi.string().required(),
+      position_of_the_responsible_for_the_collection_sector: Joi.string().required(),
+      taxman_position: Joi.string().required(),
+      taxman_name: Joi.string().required(),
+      taxman_registration: Joi.string().required(),
+      technical_contact_name: Joi.string().required(),
+      technical_telephone: Joi.number().min(8).required(),
+      technical_cellphone: Joi.number().min(9).required(),
+      contact_email: Joi.string().required(),
+      prefecture_telephone: Joi.number().min(8).required(),
+      prefecture_email: Joi.string().required(),
+    }),
+  }),
   PrefectureController.editPrefecture
 );
 
