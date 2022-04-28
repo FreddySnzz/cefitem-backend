c3931670a36d1c4b8e5ce94a8a8bea0763dca3e7
FreddySnzz <fredsonluiz.03@gmail.com>
2022-04-28 16:01:01 -0300

    fix: connection with db

--- src/config/db.js
+++ src/config/db.js
@@ -7,11 +7,11 @@ require("dotenv").config({
 
 module.exports = {
   //url: process.env.DEV_DATABASE_URL,
-  username: 'root',
-  password: 'supersecret',
-  database: 'cefitem-backend',
+  username: 'wlissesmenezes_staging',
+  password: 'kGGnN95q99Sh',
+  database: 'wlissesmenezes_cefitem_staging',
   // host: 'mysql_db', // Docker Comunication local
-  host: '127.0.0.1', // local
+  host: '207.244.244.74', // local
   dialect: process.env.DB_DIALECT || "mysql",
   explicit_defaults_for_timestamp: false,
   define: {
