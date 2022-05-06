3ec2b261b922414b87b7ad19c8237a5af3e4e92e
Andre Oliveira <andreluisoliveira013@gmail.com>
2022-05-06 18:50:00 -0300

    fix: added and use cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/UserRouter');
@@ -24,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
