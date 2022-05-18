2d1bc6df4fe2b7a59bbcb3ab4cf0e72fe36e4664
FreddySnzz <fredsonluiz.03@gmail.com>
2022-05-17 23:35:35 -0300

    feat: created admin middleware

---
+++ src/middleware/AdminMiddleware.js
@@ -0,0 +1,28 @@
+const { getJWTBody } = require('../functions/auth/getJWTBody');
+const { Admin, Contributor } = require('../models');
+
+module.exports = {
+  async ValidateADM (request, response, next) {
+    try {
+      let getToken = req.headers['authorization'];
+      let getId = await getJWTBody(getToken);
+      let verifyUser = await Admin.findOne({ raw: true,
+        where: { id: getId }
+      });
+
+      if (verifyUser == null || verifyUser == undefined) {
+
+        response.status(401).json({ message: 'Not Authorized' });
+
+      } else {
+        
+        next();
+
+      };
+
+    } catch ( error ) {
+      response.status(500).json({ error: error })
+    };
+  },
+
+}
