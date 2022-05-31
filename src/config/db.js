1f610b9248470632c0bbe29d0d58cd0deed32793
FreddySnzz <fredsonluiz.03@gmail.com>
2022-05-31 13:53:39 -0300

    fix: adjust in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,19 +5,40 @@ require("dotenv").config({
     path.resolve(__dirname, "../../.env"),
 });
 
-module.exports = {
-  //url: process.env.DEV_DATABASE_URL,
-  username: 'wlissesmenezes_staging',
-  password: 'kGGnN95q99Sh',
-  database: 'wlissesmenezes_cefitem_staging',
-  // host: 'mysql_db', // Docker Comunication local
-  host: '207.244.244.73', // local
-  dialect: process.env.DB_DIALECT || "mysql",
-  explicit_defaults_for_timestamp: false,
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
-  logging: false,
-  port: process.env.PORTDB || 3306
-};
+if (process.env.ENVIRONMENT == 'development') {
+  module.exports = {
+    //url: process.env.DEV_DATABASE_URL,
+    username: 'wlissesmenezes_staging',
+    password: 'kGGnN95q99Sh',
+    database: 'wlissesmenezes_cefitem_staging',
+    // host: 'mysql_db', // Docker Comunication local
+    host: '207.244.244.73', // local
+    dialect: process.env.DB_DIALECT || "mysql",
+    explicit_defaults_for_timestamp: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+    logging: false,
+    port: process.env.PORTDB || 3306
+  };
+  
+} else {
+  module.exports = {
+    //url: process.env.DEV_DATABASE_URL,
+    username: 'wlissesmenezes_staging',
+    password: 'kGGnN95q99Sh',
+    database: 'wlissesmenezes_cefitem_production',
+    // host: 'mysql_db', // Docker Comunication local
+    host: '207.244.244.73', // local
+    dialect: process.env.DB_DIALECT || "mysql",
+    explicit_defaults_for_timestamp: false,
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+    logging: false,
+    port: process.env.PORTDB || 3306
+  };
+}
+
